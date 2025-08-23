c697fd2d026abff126354d09fe166dceebfe0345
Andrei Vinca <andrei.vinca@outlook.com>
2025-08-23 21:01:51 +0300

    Updates

--- packages/demo/src/examples/basic/basic.tsx
+++ packages/demo/src/examples/basic/basic.tsx
@@ -7,7 +7,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import { useMemo, useState } from 'react';
 
 
-const headCells: ColumnDef<{id: number, name: string}>[] = [{
+const headCells: ColumnDef[] = [{
 	id: 'id',
 	header: 'ID',
 	cell: (row: any) => row.id,
